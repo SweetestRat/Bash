e7acd069b251f774318d56b178fe8dc6cbc9016f
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-09-25 17:09:04 +0300

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -4,11 +4,11 @@ is_numeric='^[+-]?[0-9]+$'
 
 if [[ $1 =~ $is_numeric ]]
 then
-	if (( $1 >= 0 )) && (( $1 <= 9 ))
+	if (( $1 >= 0 )) && (( $1 <= 255 ))
 	then
 		exit $1
 	else
-		echo "Error: $1 is not in range [0:9]"
+		echo "Error: $1 is not in range [0:255]"
 		exit
 	fi
 else
