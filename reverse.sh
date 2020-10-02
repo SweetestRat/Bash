e475555142570b30d6c06ac3ae2ada84779c6757
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-10-02 12:21:37 +0300

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -8,16 +8,28 @@ then
 		then
 			if [ -e $2 ]
 			then
-				if [ "$1" == "$2" ]
+				if [ -w $2 ]
 				then
-					rev $1 | tac >TMP
-					mv TMP "$2"
+					if [ "$1" == "$2" ]
+					then
+						rev $1 | tac >TMP
+						mv TMP "$2"
+					else
+						rev $1 | tac >$2
+					fi
 				else
-					rev $1 | tac >$2
+					echo "Error: write permission denied :("
+					exit 3
 				fi
 			else
-				touch "$2"
-				rev $1 | tac >$2
+				if [[ -w $2 && -r $2 ]]
+				then
+					touch "$2"
+					rev $1 | tac >$2
+				else
+					echo "Error: no read or write permission"
+					exit 4
+				fi
 			fi
 		else
 			echo "Error: File '$1' is empty"
